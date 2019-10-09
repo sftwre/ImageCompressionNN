516b3bd4e608601223a5ed55189143ddbec87f33
Isaac Buitrago <mightyking7@gmail.com>
2019-10-08 23:22:08 -0500

    Passed image through encoder and reconstructed with decoder

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,16 @@
 import numpy as np
 import PIL
+from models import Encoder, Decoder
+from torchvision import transforms
+import torch
+
+TEST_TRANSFORMS_256 = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+encoder = Encoder()
+decoder = Decoder()
 
 def encode(img, bottleneck):
     """
@@ -8,8 +19,14 @@ def encode(img, bottleneck):
     bottleneck: an integer from {4096,16384,65536}
     return: a numpy array less <= bottleneck bytes
     """
-    raise NotImplementedError('encode')
-    
+    img = TEST_TRANSFORMS_256(img).unsqueeze(0)
+
+    with torch.no_grad():
+        res = encoder.forward(img.cuda())
+
+    return res
+
+
 def decode(x, bottleneck):
     """
     Your code here
@@ -17,6 +34,10 @@ def decode(x, bottleneck):
     bottleneck: an integer from {4096,16384,65536}
     return a 256x256 PIL Image
     """
-    
-    
-    raise NotImplementedError('decode')
+
+    img = torch.from_numpy(x).float().cuda().reshape(1, 64, 32, 32)
+
+    with torch.no_grad():
+        returned = decoder.forward(img) * bottleneck
+
+    return returned
