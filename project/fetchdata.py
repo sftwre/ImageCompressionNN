a5761dd658b6e3c567ec484aeb3f929760641e71
Isaac Buitrago <mightyking7@gmail.com>
2019-10-09 20:53:07 -0500

    Parallelized fetch data

--- project/fetchdata.py
+++ project/fetchdata.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import wget
 import argparse
+import multiprocessing as mp
 
-def fetch_data(csv, save_path):
+batch = 0
+
+def fetch_data(csv, save_path, num_images):
     """
     Reads csv file into pandas data frame and downloads each image
     from the url specified in the TIFF column.
@@ -11,7 +14,20 @@ def fetch_data(csv, save_path):
 
     df = pd.read_csv(csv)
 
-    for url in df.TIFF: wget.download(url, out=save_path)
+    global batch
+
+    image_urls = list(df['TIFF'])
+
+    if batch < len(image_urls):
+        urls = image_urls[batch: batch + num_images]
+
+    else:
+        return
+
+    batch += num_images
+
+    # download images
+    for url in urls: wget.download(url, out=save_path)
 
 
 def main():
@@ -19,11 +35,15 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--csv", type=str, required=True)
     parser.add_argument("--save_path", type=str, required=True)
+    parser.add_argument("--chunk_size", type=int, default=250)
 
     args = parser.parse_args()
 
+    # create thread pool
+    pool = mp.Pool(mp.cpu_count())
+
     # read data from csv and download images
-    fetch_data(args.csv, args.save_path)
+    pool.apply(fetch_data, args=(args.csv, args.save_path, args.chunk_size))
 
 if __name__ == "__main__":
     main()
