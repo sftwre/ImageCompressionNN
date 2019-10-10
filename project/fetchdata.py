41316f6a0908317bdcd79f564b05c02203bb51de
Isaac Buitrago <mightyking7@gmail.com>
2019-10-09 21:52:31 -0500

    applied fetch_data to each worker in pool. Moved dataframe to main

--- project/fetchdata.py
+++ project/fetchdata.py
@@ -3,31 +3,16 @@ import wget
 import argparse
 import multiprocessing as mp
 
-batch = 0
 
-def fetch_data(csv, save_path, num_images):
-    """
-    Reads csv file into pandas data frame and downloads each image
-    from the url specified in the TIFF column.
-    :return:
-    """
-
-    df = pd.read_csv(csv)
-
-    global batch
-
-    image_urls = list(df['TIFF'])
-
-    if batch < len(image_urls):
-        urls = image_urls[batch: batch + num_images]
-
-    else:
-        return
-
-    batch += num_images
-
-    # download images
-    for url in urls: wget.download(url, out=save_path)
+def fetch_data(urls, save_path):
+   """
+   :param urls: list of urls to images 
+   :param save_path: path to place downloaded images
+   :param num_images:
+   :return: 
+   """
+   # download images
+   for url in urls: wget.download(url, out=save_path)
 
 
 def main():
@@ -35,15 +20,25 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--csv", type=str, required=True)
     parser.add_argument("--save_path", type=str, required=True)
-    parser.add_argument("--chunk_size", type=int, default=250)
+    parser.add_argument("--num_images", type=int, default=250)
 
     args = parser.parse_args()
 
     # create thread pool
     pool = mp.Pool(mp.cpu_count())
 
-    # read data from csv and download images
-    pool.apply(fetch_data, args=(args.csv, args.save_path, args.chunk_size))
+    # read image urls from csv
+    df = pd.read_csv(args.csv)
+
+    # download num_images for each process
+    batch = 0
+    num_images = args.num_images
+    image_urls = list(df.TIFF)
+
+    for p in range(mp.cpu_count()):
+        pool.apply_async(fetch_data, args=(image_urls[batch: batch + num_images], args.save_path))
+        batch += num_images
+
 
 if __name__ == "__main__":
     main()
